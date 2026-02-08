8509881c2a3cbaad7fbf1cc5aaba0187cb2a2d4d
Bohdan Vorobiei <vbs@ukr.net>
2026-02-08 20:16:43 +0200

    task completed

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,32 @@
 package mate.academy
 
-class FileService {
-    fun readFile(fileName: String): Result<String> {
+import java.io.File
+import java.io.IOException
 
+class FileService {
+
+    fun readFile(fileName: String): Result<String> {
+        return runCatching {
+            val file = File(fileName)
+
+            if (!file.exists() || !file.isFile) {
+                throw IOException("File does not exist or is not a valid file $fileName")
+            }
+
+            file.readText().uppercase()
+        }
     }
 
     fun processFileContent(fileName: String): String {
-
+        return readFile(fileName)
+            .fold(
+                onSuccess = { content ->
+                    "Uppercase content:\n$content"
+                },
+                onFailure = { exception ->
+                    "Error: Cannot read file - ${exception.message}"
+                }
+            )
     }
 }
+
